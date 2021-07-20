fe09d144ba83a1a7752678a83a872ffbd31a182d
wlarson30@gmail.com <wlarson30@gmail.com>
2021-07-20 04:02:59 -0400

    Added build rule for the release version of the application

--- gcc.sh
+++ gcc.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-
-mkdir build;
-mkdir build/debug;
-mkdir build/release;
-cmake -S . -B ./build/debug -DCMAKE_CXX_FLAGS_DEBUG:STRING="-g -Wall -Wextra"
+cmake -S . -B ./build/gcc/debug -DCMAKE_BUILD_TYPE:STRING="Debug" -DCMAKE_CXX_FLAGS_DEBUG:STRING="-g -Wall -Wextra"
+cmake -S . -B ./build/gcc/release -DCMAKE_BUILD_TYPE:STIRNG="Release" 
 
