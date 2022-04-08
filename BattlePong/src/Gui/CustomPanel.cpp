2711fac1bc0550f86cdbeb3cf65b01d8f87d11d7
Yanson <wlarson30@gmail.com>
2022-04-08 16:05:52 -0400

    Fixed the OptionsPanel. Now the scrollbar will not overlap the elemnts in the panel

--- BattlePong/src/Gui/CustomPanel.cpp
+++ BattlePong/src/Gui/CustomPanel.cpp
@@ -19,106 +19,106 @@ namespace Gui
         mainLayout = tgui::VerticalLayout::create();
         mainLayout->setSize("100%", "100%");
         this->add(mainLayout);
-       
-        //Create tab layout 
+
+        //Create tab layout
         tabLayout = tgui::HorizontalLayout::create();
         tabLayout->setSize("100%", "10%");
         add(tabLayout);
-        
-        //Create the content pane 
-        contentPane.reset(new ContentPane()); 
-        contentPane->setSize("100%", "80%"); 
-        contentPane->setPosition(0, "10%"); 
+
+        //Create the content pane
+        contentPane.reset(new ContentPane());
+        contentPane->setSize("100%", "80%");
+        contentPane->setPosition(0, "10%");
         contentPane->setHorizontalScrollbarPolicy(tgui::Scrollbar::Policy::Never);
-        contentPane->getRenderer()->setBackgroundColor(sf::Color::Transparent); 
-        add(contentPane); 
+        contentPane->getRenderer()->setBackgroundColor(sf::Color::Transparent);
+        add(contentPane);
 
         //Create button layout
         buttonLayout = tgui::HorizontalLayout::create();
         buttonLayout->setSize("100%", "10%");
-        buttonLayout->setPosition(0, "90%"); 
+        buttonLayout->setPosition(0, "90%");
         add(buttonLayout);
     }
-    
-    
+
+
     void CustomPanel::resize(const int &width, const int &height)
     {
         this->setSize(width / 2, height / 2);
-        this->setPosition(width / 4, height / 4); 
+        this->setPosition(width / 4, height / 4);
     }
 
     std::shared_ptr<ContentPane> CustomPanel::getContentPane()
     {
-       return contentPane; 
+       return contentPane;
     }
 
     CustomPanel::~CustomPanel()
     {
         //dtor
     }
-    
-    
+
+
 
     ContentPane::ContentPane()
     {
     }
 
 
-    
+
     bool ContentPane::append(const tgui::String &label, std::shared_ptr<tgui::Widget> widget)
     {
-        
-        //Create layout 
+
+        //Create layout
         tgui::HorizontalLayout::Ptr layout = tgui::HorizontalLayout::create();
         append(layout);
 
         //Create label
-        layout->add(tgui::Label::create(label)); 
-        
-        //Add widget to layout 
+        layout->add(tgui::Label::create(label));
+
+        //Add widget to layout
         layout->add(widget, label);
-        
-        return true; 
+
+        return true;
     }
-   
+
     bool ContentPane::append(tgui::HorizontalLayout::Ptr layout)
     {
         //layouts.push_back(layout);
-        layout->setSize("100%", HEIGHT);
-        layout->setPosition(0, count * HEIGHT); 
-        ++count; 
-        add(layout); 
+        layout->setSize("100% - 50", HEIGHT);
+        layout->setPosition(25, count * HEIGHT); 
+        ++count;
+        add(layout);
         return true;
     }
 
 
     void ContentPane::appendHeader(const tgui::String &header)
     {
-        
-        //Create layout 
+
+        //Create layout
         tgui::HorizontalLayout::Ptr layout = tgui::HorizontalLayout::create();
         append(layout);
 
         tgui::Label::Ptr label = tgui::Label::create(header);
-        //label->getRenderer()->setBackgroundColor(sf::Color(100,100,100, 100)); 
+        //label->getRenderer()->setBackgroundColor(sf::Color(100,100,100, 100));
         label->setHorizontalAlignment(tgui::Label::HorizontalAlignment::Center);
         label->setVerticalAlignment(tgui::Label::VerticalAlignment::Center);
         label->setTextSize(20);
         layout->add(label);
-        
+
     }
-    
+
     void ContentPane::append(const tgui::String &label, std::shared_ptr<tgui::CheckBox> check)
     {
         tgui::HorizontalLayout::Ptr layout = tgui::HorizontalLayout::create();
         append(layout);
 
         tgui::Panel::Ptr panel1 = tgui::Panel::create();
-        panel1->getRenderer()->setBackgroundColor(sf::Color::Transparent); 
+        panel1->getRenderer()->setBackgroundColor(sf::Color::Transparent);
         panel1->add(tgui::Label::create(label));
 
         tgui::Panel::Ptr panel2 = tgui::Panel::create();
-        panel2->getRenderer()->setBackgroundColor(sf::Color::Transparent); 
+        panel2->getRenderer()->setBackgroundColor(sf::Color::Transparent);
         panel2->add(check);
 
         layout->add(panel1);
@@ -128,7 +128,7 @@ namespace Gui
 
     void ContentPane::appendSpace()
     {
-        //Create layout 
+        //Create layout
         tgui::HorizontalLayout::Ptr layout = tgui::HorizontalLayout::create();
         append(layout);
 
