b602ad42d8325de3c4ce25ef40c9557e6e264e71
Yanson <wlarson30@gmail.com>
2022-09-13 11:30:35 -0400

    Added copyright notice

--- BattlePong/include/Box2D/DebugDraw.h
+++ BattlePong/include/Box2D/DebugDraw.h
@@ -1,14 +1,31 @@
 #ifndef DEBUGDRAW_H
 #define DEBUGDRAW_H
-#include <Box2D/Box2D.h>
-#include <SFML/Graphics/VertexArray.hpp>
-#include <SFML/Graphics/Drawable.hpp>
 /***************************************************************************
 *   @author Wayne J Larson Jr.
 *   @date   2/6/17
 *   @brief  This class is used for drawing box2d objects with SFML.
 ***************************************************************************/
 
+/*************************************************************************
+*                           COPYRIGHT NOTICE
+* Battle Pong is a clone of the classic pong game.
+* Copyright (C) 2017 Wayne J Larson Jr. 
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 3 as 
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+***************************************************************************/
+#include <Box2D/Box2D.h>
+#include <SFML/Graphics/VertexArray.hpp>
+#include <SFML/Graphics/Drawable.hpp>
 
 namespace sf
 {
--- BattlePong/include/Game.h
+++ BattlePong/include/Game.h
@@ -1,5 +1,22 @@
 #ifndef PONG_ENGIN_H
 #define PONG_ENGIN_H
+/*************************************************************************
+*                           COPYRIGHT NOTICE
+* Battle Pong is a clone of the classic pong game.
+* Copyright (C) 2017 Wayne J Larson Jr. 
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 3 as 
+* published by the Free Software Foundation.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+* 
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+***************************************************************************/
 
 #include <GameUtilities/Engin/Engin.h>
 #include <GameUtilities/Event/EventManager.h>
